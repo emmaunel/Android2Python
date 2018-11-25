ca87ca97139707afa70ba2924272e73d145cf04b
Emmanuel <aadewale123@gmail.com>
2018-11-24 22:16:48 -0500

    Initial commit

---
+++ app/src/main/java/com/wordpress/ayo218/android_client/MainActivity.java
@@ -0,0 +1,58 @@
+package com.wordpress.ayo218.android_client;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText text = findViewById(R.id.editText);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edit = text.getText().toString();
+                new Connect2Server().execute(edit);
+                text.getText().clear();
+            }
+        });
+    }
+}
+
+
+class Connect2Server extends AsyncTask<String, Void, Void>{
+
+    @Override
+    protected Void doInBackground(String... strings) {
+        try {
+            try {
+                Socket socket = new Socket("192.168.1.38", 8888);
+                PrintWriter out = new PrintWriter(
+                        new OutputStreamWriter(
+                                socket.getOutputStream()));
+
+                out.print(strings[0]);
+                out.flush();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        } catch (Exception e){
+            return null;
+        }
+        return null;
+    }
+}
